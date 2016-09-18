ec89abcb980cd5c3f1a5c2033c72f8dbfbce0a03
Darren King <darrenbrettking@hotmail.com>
2016-09-18 14:11:38 -0700

    Built feed model

--- chirpr/src/app/feed/feed.component.ts
+++ chirpr/src/app/feed/feed.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  chirps = [
+
+  { body: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', author: 'Glen', avatar: 'glen.jpg', date: new Date(), rechirps: ['Joe'], favorites: [] },
+  { body: 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight', author: 'Joe', avatar: 'joe.jpg', date: new Date(), rechirps: [], favorites: ['Mary'] },
+  { body: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', author: 'Mary', avatar: 'mary.jpg', date: new Date(), rechirps: ['Glen'], favorites: ['Mary'] },
+  { body: 'People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on each other, like a wall of mini stones', author: '../public/avatars/Glen', avatar: 'glen.jpg', date: new Date(), rechirps: ['Joe', 'Mary'], favorites: [] },
+  { body: 'You can’t have great software without a great team, and most software teams behave like dysfunctional families.', author: 'Joe', avatar: 'joe.jpg', date: new Date(), rechirps: [], favorites: ['Mary', 'Glen'] },
+
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
